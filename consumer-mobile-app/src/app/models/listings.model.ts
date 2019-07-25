8b3e3b0ac95f5b5535082f414463a5307c98b377
Michael Masner <mm6md@virgina.edu>
2019-07-25 11:44:45 +0200

    API adjustments for properties

---
+++ consumer-mobile-app/src/app/models/listings.model.ts
@@ -0,0 +1,11 @@
+export class Listing {
+
+    id: number;
+    providerId: number;
+    name: string;
+    imgUrl: string;
+    listingDescription: string;
+    location: string;
+    price: number;
+
+}
---
+++ consumer-mobile-app/src/app/services/listings.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Listing } from '../models/listings.model';
+import { HttpClient } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListingsService {
+  public listing = new Listing();
+
+  constructor(private http: HttpClient, private navCtrl: NavController) { }
+
+  create(listing: Listing){
+    return new Promise((resolve, reject) =>{
+        this.http.post('http://localhost:2000/api/property/', listing).subscribe(response => {
+            resolve(response);
+        }),
+        err =>{
+            console.log(err);
+            reject(err.msg);
+        }
+    });
+}
+
+getAll(){
+    return new Promise((resolve, reject) =>{
+        this.http.get('http://localhost:2000/api/property/').subscribe(response => {
+            resolve(response);
+        }),
+        err =>{
+            console.log(err);
+            reject(err.msg);
+        }
+    });
+}
+getById(id){
+    return new Promise((resolve, reject) => {
+        this.http.get('http://localhost:2000/api/property/' + id).subscribe(response =>{
+            resolve(response);
+        }),
+        err =>{
+            console.log(err);
+            reject(err.msg);
+        }
+    });
+}
+updateById(id){
+    return new Promise((resolve, reject) => {
+        this.http.post('http://localhost:2000/api/user/update/' + id, id).subscribe(response =>{
+            resolve(response);
+        }),
+        err =>{
+            console.log(err);
+            reject(err.msg);
+        }
+    });
+}
+
+remove(id){
+    return new Promise((resolve, reject) => {
+        this.http.post('http://localhost:2000/api/property/delete/' + id, id).subscribe(response =>{
+            resolve(response);
+        }),
+        err =>{
+            console.log(err);
+            reject(err.msg);
+        }
+    });
+}
+};
